7e0efbcf76e98139fb2cea8d966705af130f3281
kpreisser <kpreisser@users.noreply.github.com>
2023-08-24 19:18:57 +0200

    Refactor.

--- Nibbles/Program.cs
+++ Nibbles/Program.cs
@@ -1,70 +1,16 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-
-using Nibbles.Utils;
+﻿using Nibbles.Utils;
 
 namespace Nibbles;
 
 internal class Program
 {
-    public static readonly TerminalFormatter Formatter;
+    public static readonly TerminalFormatter Formatter = new();
 
-    static Program()
+    static void Main()
     {
-        // If we are running on Windows, try to enable virtual terminal processing.
-        // On other platforms, this should be enabled by default.
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            try
-            {
-                TerminalFormatter.EnableWindowsVirtualTerminalSequences();
-            }
-            catch (Exception)
-            {
-                // Ignore. This could happen when the streams is redirected, or on
-                // Windows 10 Version 1511 and lower.
-            }
+        // Initialize the console.
+        ConsoleUtils.InitializeConsole();
 
-            try
-            {
-                TerminalFormatter.EnableWindowsVirtualTerminalSequences(stdErr: true);
-            }
-            catch (Exception)
-            {
-                // Ignore. This could happen when the streams is redirected, or on
-                // Windows 10 Version 1511 and lower.
-            }
-        }
-
-        Formatter = new TerminalFormatter();
-
-        // Set UTF-8 as output encoding for the console to support Unicode characters.
-        // UTF-8 is already the standard on Linux terminals, and on Windows it is supported
-        // starting with Windows 10 (Version 1507).
-        Console.OutputEncoding = Encoding.UTF8;
-
-        // Note however, that setting Console.InputEncoding to Encoding.UTF8 will not work
-        // on Windows when the standard input stream is not redirected,
-        // whereas Encoding.Unicode works. Therefore, on Windows we
-        // set the InputEncoding to Unicode if the stream is not redirected; otherwise we
-        // use UTF-8 to be consistent with Linux and with the output encoding.
-        if (OperatingSystem.IsWindows() && !Console.IsInputRedirected)
-            Console.InputEncoding = Encoding.Unicode;
-        else
-            Console.InputEncoding = Encoding.UTF8;
-
-        // Create a new StreamReader with a large buffer so that the user can enter more than
-        // 254 characters when calling Console.ReadLine().
-        const int consoleBufferSize = 8192;
-        Console.SetIn(new StreamReader(Console.OpenStandardInput(consoleBufferSize),
-                Console.InputEncoding, false, consoleBufferSize, true));
-    }
-
-
-    static void Main(string[] args)
-    {
         // Run the game.
         NibblesGame.RunConsole(Formatter);
     }
--- Nibbles/Utils/ConsoleUtils.cs
+++ Nibbles/Utils/ConsoleUtils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Text;
 
 namespace Nibbles.Utils;
@@ -37,6 +39,105 @@ internal static class ConsoleUtils
             }
         }
     }
+
+    public static void InitializeConsole()
+    {
+        // If we are running on Windows, try to enable virtual terminal processing.
+        // On other platforms, this should be enabled by default.
+        if (OperatingSystem.IsWindows())
+        {
+            try
+            {
+                TerminalFormatter.EnableWindowsVirtualTerminalSequences(false);
+            }
+            catch (Win32Exception)
+            {
+                // Ignore. This could happen when the streams is redirected, or on
+                // Windows 10 Version 1511 and lower.
+            }
+
+            try
+            {
+                TerminalFormatter.EnableWindowsVirtualTerminalSequences(true);
+            }
+            catch (Win32Exception)
+            {
+                // Ignore. This could happen when the streams is redirected, or on
+                // Windows 10 Version 1511 and lower.
+            }
+        }
+
+        try
+        {
+            // Set UTF-8 as output encoding for the console to support Unicode
+            // characters.
+            // UTF-8 is already the standard on Linux terminals, and on Windows it is
+            // supported starting with Windows 10 (Version 1507).
+            //
+            // Note: On Windows, using UTF-8 means WriteFile is used instead of
+            // WriteConsoleW even if the stream is not redirected (whereas the latter
+            // would be used with Encoding.Unicode), which means that when starting a
+            // child process that changes the console codepage by calling
+            // SetConsoleCP/SetConsoleOutputCP (e.g. a command like "chcp 437"), the
+            // current console output encoding no longer matches the encoding set in
+            // Console.OutputEncoding.
+            // Therefore, in such a case you would need to call this method again
+            // after the child process has exited.
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Note however, that setting Console.InputEncoding to Encoding.UTF8 will
+            // not work on Windows when the standard input stream is not redirected
+            // (only ASCII characters would correctly be read), whereas
+            // Encoding.Unicode works. Therefore, on Windows we set the InputEncoding
+            // to Unicode if the stream is not redirected; otherwise we use UTF-8 to
+            // be consistent with Linux and with the output encoding.
+            // Note: This might have changed with recent enough Windows 10 Versions
+            // (20H2). See:
+            // https://github.com/MicrosoftDocs/Console-Docs/blob/5bf6f626dbef5da7944de725819993582ca8fc2d/docs/classic-vs-vt.md#unicode
+            if (!OperatingSystem.IsWindows() || Console.IsInputRedirected)
+            {
+                Console.InputEncoding = Encoding.UTF8;
+            }
+            else
+            {
+                Console.InputEncoding = Encoding.Unicode;
+
+                // By default, on Windows the standard input stream will only have a
+                // buffer size of 4096 (as of .NET Core 3.0), which means
+                // Console.ReadLine() reads only up to 2046 characters on Windows,
+                // which can be a problem when entering long license strings. To allow
+                // longer strings, we manually need to call OpenStandardInput(),
+                // create a StreamReader with a sufficient large buffer size using the
+                // Console's InputEncoding, and set Console.In to the new reader.
+                // This is the same mechanism that .NET does internally when first
+                // accessing Console.In (on Windows).
+                // Specifying a higher buffer size in the StreamReader constructor is
+                // working because this will determine the number of bytes that are
+                // passed to Stream.Read(...), which needs to be sufficently high for
+                // the maximum number of characters we want to read.
+                // Note that the new StreamReader is not synchronized, so only one
+                // thread at a time should read from the StreamReader.
+                // Note that this should only be done on Windows (if the stream is not
+                // redirected), but not Unix-based OSes, because it seems to interfere
+                // with the echo behavior on the console and could lead to
+                // Console.ReadLine() no longer echoing after starting a child process.
+                const int consoleBufferSize = 65536;
+                Console.SetIn(
+                        new StreamReader(
+                            Console.OpenStandardInput(consoleBufferSize),
+                            Console.InputEncoding,
+                            detectEncodingFromByteOrderMarks: false,
+                            consoleBufferSize,
+                            leaveOpen: true));
+            }
+        }
+        catch (IOException)
+        {
+            // Ignore. Can occur on Windows when no console is attached to the process
+            // (e.g. when running as service).
+        }
+    }
+
 
     [return: NotNullIfNotNull("str")]
     public static string? FixDisplayCharacters(
